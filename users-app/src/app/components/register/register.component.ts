692961f0ef5b4962b338f9bb2822513ba1c62374
Sumit K <synergy2411@outlook.com>
2022-01-17 16:53:10 +0530

    form array added

--- users-app/src/app/components/register/register.component.ts
+++ users-app/src/app/components/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -14,17 +14,52 @@ export class RegisterComponent implements OnInit {
   ]);
   password = new FormControl('', [
     Validators.required,
-    Validators.minLength(6)
+    Validators.minLength(6),
+    RegisterComponent.hasExclamation
   ]);
 
   registrationForm : FormGroup;
 
+  courses = new FormArray([]);
+  hobbies = new FormArray([]);
+
+  addHobby(){
+    const hobby = new FormGroup({
+      hobbyName : new FormControl(''),
+      frequency : new FormControl('')
+    })
+    this.hobbies.push(hobby);
+  }
+
+  addCourse(){
+    this.courses.push(new FormControl(''))
+  }
+
+  removeCourse(i: number){
+    this.courses.removeAt(i);
+  }
+
+  static hasExclamation (control : AbstractControl){
+    const hasExcl = control.value.indexOf("!") >= 0;
+    return hasExcl ? null : {hasExclamation : true}
+  }
+
   constructor(private fb : FormBuilder) {
     this.registrationForm = this.fb.group({
       username : this.username,
-      password : this.password
+      password : this.password,
+      books : new FormArray([])
     })
   }
+
+  get books(){
+    return this.registrationForm.get("books") as FormArray;
+  }
+
+  addBook(){
+    this.books.push(new FormControl(''))
+  }
+
 
   onRegister(){
     console.log(this.registrationForm);
