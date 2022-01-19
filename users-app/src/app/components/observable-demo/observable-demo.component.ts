a058f240e41f20e4107feccd684db298a69ce9f9
Sumit K <synergy2411@outlook.com>
2022-01-19 14:51:40 +0530

    fromevent added

--- users-app/src/app/components/observable-demo/observable-demo.component.ts
+++ users-app/src/app/components/observable-demo/observable-demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { interval, from, of, Subscription, fromEvent } from 'rxjs';
 
 @Component({
@@ -6,7 +6,9 @@ import { interval, from, of, Subscription, fromEvent } from 'rxjs';
   templateUrl: './observable-demo.component.html',
   styleUrls: ['./observable-demo.component.css']
 })
-export class ObservableDemoComponent{
+export class ObservableDemoComponent implements AfterViewInit{
+
+
 
   source1$ = interval(1000)
 
@@ -17,6 +19,23 @@ export class ObservableDemoComponent{
   source3$ = of("apple", 101, true, "Hello Wordl")
 
   unSub$ : Subscription;
+
+  source4$ = fromEvent(document, "click");
+
+  constructor(){
+    // this.source4$.subscribe((eventData) => {
+    //   console.log(eventData);
+    // })
+  }
+
+  @ViewChild("btnObservable") btn : ElementRef<any>;
+
+  ngAfterViewInit(): void {
+    let source6$ =fromEvent(this.btn.nativeElement, "click")
+    source6$.subscribe((data) => {
+      console.log(data)
+    })
+  }
 
   onSubscribe(){
     this.unSub$ = this.source1$.subscribe({
